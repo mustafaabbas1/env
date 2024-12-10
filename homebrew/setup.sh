18696b489f0fa0bd84e1ecb3813cb301de2659ed
Mustafa Abbas <mustafaabbas1@gmail.com>
2024-12-09 22:22:09 -0500

    adding homebrew and ohmyzsh

---
+++ homebrew/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "" >> $HOME/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
